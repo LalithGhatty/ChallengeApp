0dfad9a9aac0e208592e5583ba56ffb025bb13af
LalithGhatty <gvsklalith2005@gmail.com>
2026-03-07 19:41:15 +0530

    Updated file

--- ChallengeApp/src/main/java/com/embarkx/ChallengeApp/ChallengeController.java
+++ ChallengeApp/src/main/java/com/embarkx/ChallengeApp/ChallengeController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
+//JPA-JAVA PERSISTANCE API
 @RestController
 @RequestMapping("/challenges")
 public class ChallengeController {
